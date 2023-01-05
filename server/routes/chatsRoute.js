3a02c31adfd94ffa0f89489756854f80fd5afd94
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-01-05 14:12:33 +0700

    created new chat user

---
+++ server/routes/chatsRoute.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const Chat = require("../models/chatModel");
+const Message = require("../models/messageModel");
+const authMiddleware = require("../middlewares/authMiddleware");
+
+router.post("/create-new-chat", authMiddleware, async (req, res) => {
+  try {
+    const newChat = new Chat(req.body);
+    const savedChat = await newChat.save();
+
+    await savedChat.populate("members");
+    res.send({
+      success: true,
+      message: "Chat created successfully",
+      data: savedChat,
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: "Error creating chat",
+      error: error.message,
+    });
+  }
+});

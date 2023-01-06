42c0ee6a190531f67af01cb17dec22dacb2f03b4
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-01-06 15:15:38 +0700

    created router page
    created route page with react router dom version 6

--- client/src/pages/login/index.jsx
+++ client/src/pages/login/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const index = () => {
+const Login = () => {
   return (
     <div>
       <h1>Login Page</h1>
@@ -8,4 +8,4 @@ const index = () => {
   );
 };
 
-export default index;
+export default Login;

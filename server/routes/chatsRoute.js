bee8bab327b367e9ad0562375eecf40e190c9953
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-01-07 08:44:34 +0700

    created unread logic chat api

--- server/routes/chatsRoute.js
+++ server/routes/chatsRoute.js
@@ -47,4 +47,46 @@ router.get("get-all-chats", authMiddleware, async (res, req) => {
   }
 });
 
+router.post("/clear-unread-messages", authMiddleware, async (req, res) => {
+  try {
+    const chat = await Chat.findById(req.body.chat);
+    if (!chat) {
+      return res.send({
+        success: false,
+        message: "Chat not found",
+      });
+    }
+    const updatedChat = await Chat.findByIdAndUpdate(
+      req.body.chat,
+      {
+        unreadMessages: 0,
+      },
+      { new: true }
+    )
+      .populate("members")
+      .populate("lastMessage");
+
+    await Message.updateMany(
+      {
+        chat: req.body.chat,
+        read: false,
+      },
+      {
+        read: true,
+      }
+    );
+    res.send({
+      success: true,
+      message: "Unread messages cleared successfully",
+      data: updatedChat,
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: "Error clearing unread messages",
+      error: error.message,
+    });
+  }
+});
+
 module.exports = router;

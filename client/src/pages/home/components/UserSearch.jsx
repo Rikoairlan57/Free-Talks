efb7aa0a216669b20ba01e65feb00d021d3b4502
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-01-09 16:59:16 +0700

    created fundamental home chats and ui desain user search

---
+++ client/src/pages/home/components/UserSearch.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const UserSearch = () => {
+  return (
+    <div className="relative">
+      <input
+        type="text"
+        placeholder="Search users / chats"
+        className="rounded-xl w-full border-gray-300 pl-10 text-gray-500 h-14"
+      />
+      <i className="ri-search-line absolute top-4 left-4 text-gray-500"></i>
+    </div>
+  );
+};
+
+export default UserSearch;

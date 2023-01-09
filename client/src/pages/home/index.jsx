8b99b3d13e14ff1fc7b9bc0c2dea62740a0c3f74
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-01-09 18:53:22 +0700

    created user list
    created user list and user search implementation

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 import UserSearch from "./components/UserSearch";
+import UsersList from "./components/UsersList";
 
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:6000");
 const Home = () => {
+  const [searchKey, setSearchKey] = React.useState("");
+  const { selectedChat, user } = useSelector((state) => state.userReducer);
+  const [onlineUsers, setOnlineUsers] = React.useState([]);
+  useEffect(() => {
+    // join the room
+    if (user) {
+      socket.emit("join-room", user._id);
+      socket.emit("came-online", user._id);
+
+      socket.on("online-users-updated", (users) => {
+        setOnlineUsers(users);
+      });
+    }
+  }, [user]);
   return (
     <div className="flex gap-5">
       <div className="w-96">
-        <UserSearch />
-        <h1>Home Page</h1>
+        <UserSearch searchKey={searchKey} setSearchKey={setSearchKey} />
+        <UsersList
+          searchKey={searchKey}
+          socket={socket}
+          onlineUsers={onlineUsers}
+        />
       </div>
     </div>
   );
